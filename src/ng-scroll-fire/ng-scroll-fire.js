110f375f29b0ac1b41c0ffdc4ef37af22bbe1aa0
Amit Mutreja <amitmutrezas@gmail.com>
2016-09-22 00:00:37 +0530

    [Amit] Initial release

---
+++ src/ng-scroll-fire/ng-scroll-fire.js
@@ -0,0 +1,19 @@
+angular.module('ngScrollFire').directive('ngScrollFire', function() {
+  return {
+    restrict: 'A',
+    link: function(scope, element, attrs) {
+      var raw;
+      raw = element[0];
+      console.log('loading directive');
+      element.bind('scroll', function() {
+        console.log('in scroll');
+        console.log(raw.scrollTop + raw.offsetHeight);
+        console.log(raw.scrollHeight);
+        if (raw.scrollTop + raw.offsetHeight >= raw.scrollHeight) {
+          console.log('I am at the bottom');
+          scope.$apply(attrs.ngScrollFire);
+        }
+      });
+    }
+  };
+});
